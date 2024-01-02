4a3dac17a5ecf50b0b2ea1e7da92b1bd2b5d0ba0
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2024-01-02 15:55:14 -0500

    Create strange-advertising.py

---
+++ strange-advertising.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    p = 5
+    t = 0
+    for _ in range(n):
+        t += p//2
+        p = 3*(p//2)
+    return t
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
