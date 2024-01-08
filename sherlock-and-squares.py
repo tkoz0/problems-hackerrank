02101463646a65bb8382e7a8008592f6479b8649
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2024-01-08 14:54:37 -0500

    Create sherlock-and-squares.py

---
+++ sherlock-and-squares.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    # Write your code here
+    ra = int(math.sqrt(a))
+    rb = int(math.sqrt(b))
+    ret = rb-ra
+    if ra*ra == a:
+        ret += 1
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
