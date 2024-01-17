5472e5f0c8b1a8bc6740015ac5bb48417cc09349
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2024-01-17 05:48:24 -0500

    Create lisa-workbook.py

---
+++ lisa-workbook.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    # Write your code here
+    ret = 0
+    page = 0
+    for chapter in range(1,n+1):
+        page += 1
+        problems = 0
+        for problem in range(1,1+arr[chapter-1]):
+            if problems == k:
+                problems = 0
+                page += 1
+            if problem == page:
+                ret += 1
+            problems += 1
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
