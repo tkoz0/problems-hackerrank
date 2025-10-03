c2541f37d96564f44b0fd4f04415a79726e9378e
TKoz <13438573+tkoz0@users.noreply.github.com>
2025-10-03 02:37:56 -0400

    Create jumping-on-the-clouds.py

---
+++ jumping-on-the-clouds.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    i = 0
+    steps = 0
+    while i < len(c)-1:
+        if i+2 < len(c) and c[i+2] == 0:
+            i += 2
+        else:
+            assert c[i+1] == 0
+            i += 1
+        steps += 1
+    return steps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
