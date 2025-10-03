dda40bb33fccb8f3ec0958f23b30d11ee7a239e6
TKoz <13438573+tkoz0@users.noreply.github.com>
2025-10-03 02:15:12 -0400

    Create repeated-string.py

---
+++ repeated-string.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    acount = sum(1 for c in s if c == 'a')
+    ret = acount * (n // len(s))
+    for c in s[:n%len(s)]:
+        if c == 'a':
+            ret += 1
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
