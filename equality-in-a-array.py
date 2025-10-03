10c995040644b73a83e34c55e4e3b92467a3a0e6
TKoz <13438573+tkoz0@users.noreply.github.com>
2025-10-03 02:18:23 -0400

    Create equality-in-a-array.py

---
+++ equality-in-a-array.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    counts = dict()
+    for n in arr:
+        if n not in counts:
+            counts[n] = 0
+        counts[n] += 1
+    return len(arr)-max(counts.values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
